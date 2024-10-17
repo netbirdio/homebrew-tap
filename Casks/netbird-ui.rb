77800aeceaa6bb9d499b934c338a7769c7a3c046
Maycon Santos <mlsmaycon@gmail.com>
2024-10-17 16:31:50 +0200

    fix zip path

--- Casks/netbird-ui.rb
+++ Casks/netbird-ui.rb
@@ -8,11 +8,11 @@ cask "netbird-ui" do
   if Hardware::CPU.intel?
       url "https://github.com/netbirdio/netbird/releases/download/v0.30.2/netbird-ui_0.30.2_darwin_amd64_signed.zip"
       sha256 "a644c7da71595872b154ce56a9ecc5aad68fb8f3295d966899b5b5f0aeb607d9"
-      app "netbird_ui_darwin_amd64", target: "Netbird UI.app"
+      app "netbird_ui_darwin", target: "Netbird UI.app"
   else
       url "https://github.com/netbirdio/netbird/releases/download/v0.30.2/netbird-ui_0.30.2_darwin_arm64_signed.zip"
       sha256 "66954d82867c23f207dff58cecb07b0174f76c953bc29d031fccf07f7f439e57"
-      app "netbird_ui_darwin_arm64", target: "Netbird UI.app"
+      app "netbird_ui_darwin", target: "Netbird UI.app"
   end
 
   depends_on formula: "netbird"
