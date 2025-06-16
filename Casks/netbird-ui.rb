f1494dacb08a4eaa48cc9124aaa257a81fc63a06
mlsmaycon <dev@netbird.io>
2025-06-16 19:30:05 +0000

    Add changes

--- Casks/netbird-ui.rb
+++ Casks/netbird-ui.rb
@@ -3,15 +3,15 @@
 
 # Netbird's UI Client Cask Formula
 cask "netbird-ui" do
-  version "0.46.0"
+  version "0.47.0"
 
   if Hardware::CPU.intel?
-      url "https://github.com/netbirdio/netbird/releases/download/v0.46.0/netbird-ui_0.46.0_darwin_amd64_signed.zip"
-      sha256 "8628642885079e750b2b539ff5eeeb4ffd406962612ccf40fa51ec7595ea788a"
+      url "https://github.com/netbirdio/netbird/releases/download/v0.47.0/netbird-ui_0.47.0_darwin_amd64_signed.zip"
+      sha256 "d7776a39b79ae14ac151ed7614cba5cc960816c490bbf3dfcee482428408796d"
       app "netbird_ui_darwin", target: "Netbird UI.app"
   else
-      url "https://github.com/netbirdio/netbird/releases/download/v0.46.0/netbird-ui_0.46.0_darwin_arm64_signed.zip"
-      sha256 "ba6c5178a504e3e7c55f4df0817494e43431c97dfbcd5198470408e8d4377ef4"
+      url "https://github.com/netbirdio/netbird/releases/download/v0.47.0/netbird-ui_0.47.0_darwin_arm64_signed.zip"
+      sha256 "c4027fc45bf1708611140aba8bbebab364ebc421ed46e6a9ff2046e594d8c894"
       app "netbird_ui_darwin", target: "Netbird UI.app"
   end
 
